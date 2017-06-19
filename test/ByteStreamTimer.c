a827a01c7d893bc91cca09f437d9624dd4da3c86
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-20 01:19:47 +0530

    Remove debug messages

--- test/ByteStreamTimer.c
+++ test/ByteStreamTimer.c
@@ -2,7 +2,6 @@
 #include "ByteStreamTimer.h"
 
 ByteStreamTimerTaskIdentifier byte_stream_timer_set_timeout(byte_stream_timer_task task, void *argument, int milliseconds) {
-  printf("task was run\n");
   return 0;
 }
 
@@ -12,5 +11,5 @@ ByteStreamTimerTaskIdentifier byte_stream_timer_set_interval(byte_stream_timer_t
 }
 
 void byte_stream_timer_clear_timeout(ByteStreamTimerTaskIdentifier task_id) {
-  printf("task was cleared\n");
+
 }
