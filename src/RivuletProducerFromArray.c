c5b304c53a01f26aa76c5a9c7443e1266e4311f5
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-09 13:32:32 +0530

    Simplify from array producer implementation

--- src/RivuletProducerFromArray.c
+++ src/RivuletProducerFromArray.c
@@ -1,25 +1,38 @@
 #include "RivuletProducerFromArray.h"
-#include "RivuletListernerManager.h"
+#include "RivuletProducerRegistry.h"
+#include "RivuletListenerRegistry.h"
 
-static void _from_array_start (RivuletProducer *self, RivuletListener *listener) {
+static void _start (RivuletProducer *self, RivuletListener *listener) {
   RivuletProducerFromArray *producer = (RivuletProducerFromArray *) self;
   RivuletArray *array = producer->array;
   int length = rivulet_array_length (array);
   for (int i = 0; i < length; i++) {
     int *value = rivulet_array_get (array, i);
-    rivulet_listener_internal_next_get (listener) (listener, *value);
+    rivulet_listener_registry_get_next (listener->listener_type) (listener, *value);
   }
-  rivulet_listener_internal_complete_get (listener) (listener);
+  rivulet_listener_registry_get_complete (listener->listener_type) (listener);
 }
 
-static void _from_array_stop (RivuletProducer *self) {
+static void _stop (RivuletProducer *self) {
 
+}
+
+static Boolean _registered = 0;
+static RivuletListenerType _listener_type = 0;
+static RivuletProducerType _producer_type = 0;
+
+static void _register () {
+  if (_registered) return;
+  _producer_type = rivulet_producer_registry_register (_start, _stop);
+  _registered = 1;
 }
 
 RivuletProducer *rivulet_producer_from_variable_length_array (RivuletArray *array) {
   RivuletProducerFromArray *producer = xmalloc (sizeof (RivuletProducerFromArray));
   producer->array = array;
-  rivulet_producer_initialize ((RivuletProducer *) producer, _from_array_start, _from_array_stop);
+  _register ();
+  producer->listener_type = _listener_type;
+  producer->producer_type = _producer_type;
   return (RivuletProducer *) producer;
 }
 
@@ -30,6 +43,8 @@ RivuletProducer *rivulet_producer_from_array (int *array, int size) {
   for (int i = 0; i < size; i++) {
     rivulet_array_push (varray, array + i);
   }
-  rivulet_producer_initialize ((RivuletProducer *) producer, _from_array_start, _from_array_stop);
+  _register ();
+  producer->listener_type = _listener_type;
+  producer->producer_type = _producer_type;
   return (RivuletProducer *) producer;
 }
