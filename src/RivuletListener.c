4f3e7f0270f799cf61179f39c7a8bdff9662e665
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-09 13:52:04 +0530

    Fix factory

--- src/RivuletListener.c
+++ src/RivuletListener.c
@@ -1,8 +1,10 @@
 #include "RivuletListener.h"
+#include "RivuletListenerRegistry.h"
+#include "RivuletUtils.h"
 
-RivuletListener *rivulet_listener_create () {
+RivuletListener *rivulet_listener_create (rivulet_listener_next next, rivulet_listener_complete complete) {
   RivuletListener *listener = xmalloc (sizeof (RivuletListener));
   listener->producer_type = 0;
-  listener->listener_type = rivulet_listener_registry_register (_next, _complete);
+  listener->listener_type = rivulet_listener_registry_register (next, complete);
   return listener;
 }
