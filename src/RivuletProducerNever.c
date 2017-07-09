4af2e65f7b48d0e2e749bf7165fd9350b6000f03
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-09 13:50:43 +0530

    Add never producer

---
+++ src/RivuletProducerNever.c
@@ -0,0 +1,30 @@
+#include "RivuletProducerNever.h"
+#include "RivuletUtils.h"
+#include "Boolean.h"
+#include "RivuletProducerRegistry.h"
+
+static void _start (struct RivuletProducer *self, struct RivuletListener *listener) {
+
+}
+
+static void _stop (struct RivuletProducer *self) {
+
+}
+
+static Boolean _registered = 0;
+static RivuletListenerType _listener_type = 0;
+static RivuletProducerType _producer_type = 0;
+
+static void _register () {
+  if (_registered) return;
+  _producer_type = rivulet_producer_registry_register (_start, _stop);
+  _registered = 1;
+}
+
+RivuletProducer *rivulet_producer_never_create () {
+  RivuletProducer *producer = xmalloc (sizeof (RivuletProducer));
+  _register ();
+  producer->listener_type = _listener_type;
+  producer->producer_type = _producer_type;
+  return producer;
+}
