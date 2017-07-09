909b4d2c1a188ad00a072778008daeeab721466b
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-09 12:57:29 +0530

    Implement operator functions

---
+++ src/RivuletOperator.c
@@ -0,0 +1,24 @@
+#include "RivuletOperator.h"
+#include "RivuletListenerRegistry.h"
+
+void rivulet_operator_in_add (void *operator) {
+  RivuletOperator *self = operator;
+  RivuletListener *listener = operator;
+  self->in->_add (self->in, listener);
+}
+
+void rivulet_operator_in_remove (void *operator) {
+  RivuletOperator *self = operator;
+  RivuletListener *listener = operator;
+  self->in->_remove (self->in, listener);
+}
+
+void rivulet_operator_out_next (void *operator, int value) {
+  RivuletOperator *self = operator;
+  rivulet_listener_registry_get_next (self->out->listener_type) ((RivuletListener *) self->out, value);
+}
+
+void rivulet_operator_out_complete (void *operator) {
+  RivuletOperator *self = operator;
+  rivulet_listener_registry_get_complete (self->out->listener_type) ((RivuletListener *) self->out);
+}
