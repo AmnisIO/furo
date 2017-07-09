07b91335d6d918ff6f4059d8d0629b97bab66336
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-09 13:24:01 +0530

    Simplify filter

--- src/RivuletFilter.c
+++ src/RivuletFilter.c
@@ -1,39 +1,48 @@
 #include "RivuletFilter.h"
+#include "RivuletListenerRegistry.h"
+#include "RivuletProducerRegistry.h"
 
-static void _start (RivuletProducerInternal *self, RivuletListenerInternal *out) {
+static void _start (RivuletProducer *self, RivuletListener *out) {
   RivuletFilter *operator = (RivuletFilter *) self;
   RivuletStream *stream = (RivuletStream *) out;
   operator->out = stream;
-  operator->in->_add (operator->in, (RivuletListenerInternal *) operator);
+  rivulet_operator_in_add (operator);
 }
 
-static void _stop (RivuletProducerInternal *self) {
+static void _stop (RivuletProducer *self) {
   RivuletFilter *operator = (RivuletFilter *) self;
-  operator->in->_remove (operator->in, (RivuletListenerInternal *) operator);
+  rivulet_operator_in_remove (operator);
   operator->out = NULL;
 }
 
-static void _next (RivuletListenerInternal *self, int value) {
+static void _next (RivuletListener *self, int value) {
   RivuletFilter *operator = (RivuletFilter *) self;
   Boolean passes = operator->filter (value);
   if (!passes) return;
-  operator->out->_next ((RivuletListenerInternal *) operator->out, value);
+  rivulet_operator_out_next (operator, value);;
 }
 
-static void _complete (RivuletListenerInternal *self) {
+static void _complete (RivuletListener *self) {
   RivuletFilter *operator = (RivuletFilter *) self;
-  operator->out->_complete ((RivuletListenerInternal *) operator->out);
+  rivulet_operator_out_complete (operator);;
 }
 
-RivuletFilter *rivulet_filter_create (RivuletStream *in, rivulet_filter_function filter) {
+static Boolean _registered = 0;
+static RivuletListenerType _listener_type = 0;
+static RivuletProducerType _producer_type = 0;
+
+static void _register () {
+  if (_registered) return;
+  _listener_type = rivulet_listener_registry_register (_next, _complete);
+  _producer_type = rivulet_producer_registry_register (_start, _stop);
+  _registered = 1;
+}
+RivuletProducer *rivulet_filter_create (RivuletStream *in, rivulet_filter_function filter) {
   RivuletFilter *operator = xmalloc (sizeof (RivuletFilter));
-  byte_byte_operator_initialize ((RivuletOperator *) operator);
-  operator->operation = RIVULET_OPERATOR_FILTER;
+  _register ();
+  operator->listener_type = _listener_type;
+  operator->producer_type = _producer_type;
   operator->in = in;
   operator->filter = filter;
-  operator->_start = _start;
-  operator->_stop = _stop;
-  operator->_next = _next;
-  operator->_complete = _complete;
-  return operator;
+  return (RivuletProducer *) operator;
 }
