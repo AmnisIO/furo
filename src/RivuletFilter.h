3440e96c91715d62be115fd7288f881584692c47
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-09 13:09:55 +0530

    Simplify all operator declarations

--- src/RivuletFilter.h
+++ src/RivuletFilter.h
@@ -6,17 +6,13 @@
 typedef Boolean (*rivulet_filter_function) (int value);
 
 typedef struct RivuletFilter {
-  RivuletObservableType type;
-  rivulet_producer_internal_start _start;
-  rivulet_producer_internal_stop _stop;
-  rivulet_listener_internal_next _next;
-  rivulet_listener_internal_complete _complete;
+  RivuletListenerType listener_type;
+  RivuletProducerType producer_type;
   RivuletStream *in;
   RivuletStream *out;
-  OperatorType operation;
   rivulet_filter_function filter;
 } RivuletFilter;
 
-RivuletFilter *rivulet_filter_create (RivuletStream *in, rivulet_filter_function filter);
+RivuletProducer *rivulet_filter_create (RivuletStream *in, rivulet_filter_function filter);
 
 #endif //RIVULET_FILTER_H
--- src/RivuletLast.h
+++ src/RivuletLast.h
@@ -4,18 +4,14 @@
 #include "RivuletOperator.h"
 
 typedef struct RivuletLast {
-  RivuletObservableType type;
-  rivulet_producer_internal_start _start;
-  rivulet_producer_internal_stop _stop;
-  rivulet_listener_internal_next _next;
-  rivulet_listener_internal_complete _complete;
+  RivuletListenerType listener_type;
+  RivuletProducerType producer_type;
   RivuletStream *in;
   RivuletStream *out;
-  OperatorType operation;
   Boolean _has;
   int _value;
 } RivuletLast;
 
-RivuletLast *rivulet_last_create (RivuletStream *in);
+RivuletProducer *rivulet_last_create (RivuletStream *in);
 
 #endif //RIVULET_LAST_H
