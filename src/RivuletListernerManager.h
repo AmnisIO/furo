7241cf9ee1e1a9ed975df3ada14b10ae9509df3c
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-09 12:15:13 +0530

    Remove listener manager

--- src/RivuletListernerManager.h
+++
@@ -1,9 +0,0 @@
-#ifndef RIVULET_LISTERNERMANAGER_H
-#define RIVULET_LISTERNERMANAGER_H
-
-#include "RivuletListener.h"
-
-rivulet_listener_internal_next rivulet_listener_internal_next_get (RivuletListenerInternal *listener);
-rivulet_listener_internal_complete rivulet_listener_internal_complete_get (RivuletListenerInternal *listener);
-
-#endif //RIVULET_LISTERNERMANAGER_H
