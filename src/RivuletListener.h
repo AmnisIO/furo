6a77967e1f9e53026362e1d888d72947496e161f
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-09 11:52:41 +0530

    Simplify listeners

--- src/RivuletListener.h
+++ src/RivuletListener.h
@@ -1,21 +1,11 @@
 #ifndef RIVULET_LISTENER_H
 #define RIVULET_LISTENER_H
 
-#include "RivuletListenerInternal.h"
-
 typedef struct RivuletListener {
-  RivuletObservableType type;
-  rivulet_listener_internal_next _next;
-  rivulet_listener_internal_complete _complete;
-  void (*next) (struct RivuletListener *self, int v);
-  void (*complete) (struct RivuletListener *self);
+  RivuletListenerType type;
 } RivuletListener;
 
-typedef void (*rivulet_listener_next) (struct RivuletListener *self, int v);
-typedef void (*rivulet_listener_complete) (struct RivuletListener *self);
-
-RivuletListener *
-rivulet_listener_create (rivulet_listener_next next, rivulet_listener_complete complete);
-void rivulet_listener_initialize (RivuletListener *listener, rivulet_listener_next next, rivulet_listener_complete complete);
+typedef void (*rivulet_listener_next) (RivuletListener *, int);
+typedef void (*rivulet_listener_complete) (RivuletListener *);
 
 #endif //RIVULET_LISTENER_H
