8e16d0206cea2e7261f53c6723970ad0620b8690
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-10 22:08:01 +0530

    Add Rivulet.h

---
+++ src/Rivulet.h
@@ -0,0 +1,26 @@
+#ifndef RIVULET_RIVULET_H
+#define RIVULET_RIVULET_H
+
+#include "RivuletTimer.h"
+
+#include "RivuletProducerRegistry.h"
+
+#include "RivuletProducerEmpty.h"
+#include "RivuletProducerNever.h"
+#include "RivuletProducerFromArray.h"
+#include "RivuletProducerPeriodic.h"
+
+#include "RivuletListenerRegistry.h"
+
+#include "RivuletStream.h"
+
+#include "RivuletMap.h"
+#include "RivuletMapTo.h"
+#include "RivuletFilter.h"
+#include "RivuletTake.h"
+#include "RivuletDrop.h"
+#include "RivuletLast.h"
+#include "RivuletDelay.h"
+#include "RivuletSample.h"
+
+#endif //RIVULET_RIVULET_H
